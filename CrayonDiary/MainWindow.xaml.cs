b7a3e10b884437afa1d4ef7bf49dfd5b247a9226
cseri502 <82875920+cseri502@users.noreply.github.com>
2023-02-06 23:25:04 +0100

    Add project files.

---
+++ CrayonDiary/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ModernWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CrayonDiary
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // https://github.com/Kinnara/ModernWpf/wiki/
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.Current.ApplicationTheme = ApplicationTheme.Light;
+            ThemeManager.Current.AccentColor = Color.FromRgb(53, 126, 199);
+        }
+    }
+}
