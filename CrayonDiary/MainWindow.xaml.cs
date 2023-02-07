2b5c22b46f93d6e6a158c028335f6d8513bc2bc4
cseri502 <82875920+cseri502@users.noreply.github.com>
2023-02-07 16:56:23 +0100

    Update things.

--- CrayonDiary/MainWindow.xaml.cs
+++ CrayonDiary/MainWindow.xaml.cs
@@ -21,16 +21,58 @@ namespace CrayonDiary
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly List<Student> students = new();
         public MainWindow()
         {
             // https://github.com/Kinnara/ModernWpf/wiki/
             InitializeComponent();
         }
 
+        private void SetColNames()
+        {
+            MarkGrid.Columns[0].Header = "Név";
+            MarkGrid.Columns[0].Header = "Osztály";
+            MarkGrid.Columns[0].Header = "Matematika";
+            MarkGrid.Columns[0].Header = "Irodalom";
+            MarkGrid.Columns[0].Header = "Biológia";
+            MarkGrid.Columns[0].Header = "Testnevelés";
+            MarkGrid.Columns[0].Header = "Informatika";
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             ThemeManager.Current.ApplicationTheme = ApplicationTheme.Light;
             ThemeManager.Current.AccentColor = Color.FromRgb(53, 126, 199);
+            MarkGrid.ItemsSource = students;
+            SetColNames();
+        }
+
+        private void addStudentBtn_Click(object sender, RoutedEventArgs e)
+        {
+            students.Add(new Student($"{addNewStudentInput.Text};?;0;0;0;0;0"));
+            MarkGrid.ItemsSource = students;
+            MarkGrid.Items.Refresh();
+            SetColNames();
+        }
+
+        private void addStudentClassBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedIndex = MarkGrid.SelectedIndex;
+
+            if (selectedIndex >= 0) students.ElementAt(selectedIndex).Class = changeStudentClassInput.Text;
+            else MessageBox.Show("Nem választottál ki tanulót!");
+
+            MarkGrid.Items.Refresh();
+        }
+
+        private void delStudentBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedIndex = MarkGrid.SelectedIndex;
+
+            if (selectedIndex >= 0) students.RemoveAt(selectedIndex);
+            else MessageBox.Show("Nem választottál ki tanulót!");
+
+            MarkGrid.Items.Refresh();
         }
     }
 }
---
+++ CrayonDiary/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrayonDiary
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public int Math { get; set; }
+        public int Literature { get; set; }
+        public int Biology { get; set; }
+        public int PE { get; set; }
+        public int IT { get; set; }
+
+        public Student(string line)
+        {
+            string[] part = line.Split(';');
+            Name = part[0];
+            Class = part[1];
+            Math = int.Parse(part[2]);
+            Literature= (int)int.Parse(part[3]);
+            Biology= (int)int.Parse(part[4]);
+            PE = (int)int.Parse(part[5]);
+            IT= (int)int.Parse(part[6]);
+        }
+    }
+}
